5e718005b6f93584e2808110100bcd9dc69524d4
Blaž <blaz.bocko1@gmail.com>
2024-08-07 19:45:01 +0200

    Add service for handling data

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,7 +1,6 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-import { type InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -11,15 +10,15 @@ import { type InvestmentInput } from '../investment-input.model';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  calculate = output<InvestmentInput>();
-
   enteredInitialInvestment = signal(0);
   enteredAnnualInvestment = signal(0);
   enteredExpectedReturn = signal(0);
   enteredDuration = signal(0);
 
+  private investmentService = inject(InvestmentService);
+
   onSubmit() {
-    this.calculate.emit({
+    this.investmentService.calculateInvestmentResults({
       initialInvestment: this.enteredInitialInvestment(),
       annualInvestment: this.enteredAnnualInvestment(),
       expectedReturn: this.enteredExpectedReturn(),
